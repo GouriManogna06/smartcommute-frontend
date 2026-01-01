6d139c0e18833b579928d72fa9d6c7db7a52d4e7
GouriManogna06 <gourimanogna@gmail.com>
2026-01-01 19:24:05 +0530

    Add files via upload

--- src/HyderabadMap.js
+++ src/HyderabadMap.js
@@ -1,43 +1,65 @@
 import React from "react";
-import { MapContainer, TileLayer, Polyline, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-const HyderabadMap = ({ routeData }) => {
-  const center = [17.4399, 78.4983];
+// Default marker fix
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+function HyderabadMap({ routeData }) {
+  const polyline =
+    routeData?.polyline || routeData?.routePolyline || null;
+
+  const start = routeData?.start_point || null;
+  const end = routeData?.end_point || null;
 
   return (
-    <div className="map-wrapper">
-      <MapContainer center={center} zoom={12} style={{ height: "400px", width: "100%" }}>
-        <TileLayer
-          attribution='&copy; OpenStreetMap'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    <div style={{ width: "100%", height: "450px" }}>
+      <MapContainer
+        center={[17.385, 78.486]}
+        zoom={12}
+        style={{ height: "450px", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        {routeData && (
-          <>
-            <Polyline
-              positions={routeData.points.map((p) => [p.lat, p.lon])}
-              color="blue"
-              weight={6}
-            />
+        {/* Start marker */}
+        {start && (
+          <Marker position={[start.lat, start.lon]}>
+            <Popup>Start Point</Popup>
+          </Marker>
+        )}
 
-            <Marker position={[routeData.points[0].lat, routeData.points[0].lon]}>
-              <Popup>Start</Popup>
-            </Marker>
+        {/* End marker */}
+        {end && (
+          <Marker position={[end.lat, end.lon]}>
+            <Popup>Destination</Popup>
+          </Marker>
+        )}
 
-            <Marker
-              position={[
-                routeData.points[routeData.points.length - 1].lat,
-                routeData.points[routeData.points.length - 1].lon,
-              ]}
-            >
-              <Popup>Destination</Popup>
-            </Marker>
-          </>
+        {/* Polyline */}
+        {polyline && polyline.length > 0 && (
+          <Polyline
+            positions={polyline}
+            pathOptions={{ color: "blue", weight: 5 }}
+          />
         )}
       </MapContainer>
     </div>
   );
-};
+}
 
 export default HyderabadMap;
